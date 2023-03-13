353c5a8eec8d8d83398a525bde28e879a2c70d90
Phạm Tuấn Kiệt <103875393+phamtuankietuit@users.noreply.github.com>
2023-03-13 23:06:22 +0700

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -4,8 +4,5 @@ using namespace std;
 int main()
 {
     cout<<"Toi la Kiet day, go tao lao vao day";
-    cout<<"xin chao tat ca cac ban";
-    cout<<"I'm a b*tch";
-    cout<<"Please me, daddy!!!!";
     return 0;
 }
