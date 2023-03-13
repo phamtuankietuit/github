73c37a604e003205d04dc679561aca25dbf72781
kit <21522262@gm.uit.edu.vn>
2023-03-13 23:08:04 +0700

    conflict ne

--- index.cpp
+++ index.cpp
@@ -7,5 +7,6 @@ int main()
     cout<<"xin chao tat ca cac ban";
     cout<<"I'm a b*tch";
     cout<<"Please me, daddy!!!!";
+    cout<<"Xin chao tat ca moi nguoi dang co mat tai conflict ngay hom nay";
     return 0;
 }
